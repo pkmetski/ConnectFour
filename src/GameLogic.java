2b4dba0c02f67d6489f46afe5d409ca5079e9bc6
pkmetski <pkme@itu.dk>
2014-02-22 18:00:04 +0100

    aa

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,33 +1,30 @@
-
 public class GameLogic implements IGameLogic {
-    private int x = 0;
-    private int y = 0;
-    private int playerID;
-    
-    public GameLogic() {
-        //TODO Write your implementation for this method
-    }
-	
-    public void initializeGame(int x, int y, int playerID) {
-        this.x = x;
-        this.y = y;
-        this.playerID = playerID;
-        //TODO Write your implementation for this method
-    }
-	
-    public Winner gameFinished() {
-        //TODO Write your implementation for this method
-        return Winner.NOT_FINISHED;
-    }
+	private int x = 0;
+	private int y = 0;
+	private int playerID;
 
+	public GameLogic() {
+		// TODO Write your implementation for this method
+	}
 
-    public void insertCoin(int column, int playerID) {
-        //TODO Write your implementation for this method	
-    }
+	public void initializeGame(int x, int y, int playerID) {
+		this.x = x;
+		this.y = y;
+		this.playerID = playerID;
+		// TODO Write your implementation for this method
+	}
 
-    public int decideNextMove() {
-        //TODO Write your implementation for this method
-        return 0;
-    }
+	public Winner gameFinished() {
+		// TODO Write your implementation for this method
+		return Winner.NOT_FINISHED;
+	}
 
+	public void insertCoin(int column, int playerID) {
+		// TODO Write your implementation for this method
+	}
+
+	public int decideNextMove() {
+		// TODO Write your implementation for this method
+		return 0;
+	}
 }
