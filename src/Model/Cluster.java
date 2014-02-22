d05ad97bd9105d6d7a172d107ced3fc44d776fd4
arian2ashk <arian_2ashk@yahoo.com>
2014-02-22 17:57:33 +0100

    cluster

--- src/Model/Cluster.java
+++ src/Model/Cluster.java
@@ -1,12 +1,34 @@
 package Model;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Cluster {
 
-	public Cluster() {
+	private Set<Position> positions;
 
-		ArrayList<Position> positions = new ArrayList<Position>();
+	public Cluster(Position position) {
+		this.positions = new HashSet<Position>();
+		this.positions.add(position);
 	}
 
+	public void addPosition(Position position) {
+		positions.add(position);
+	}
+
+	public boolean contains(Position position) {
+		return positions.contains(position);
+	}
+
+	public int count() {
+		return positions.size();
+	}
+
+	public Set<Position> getPositions() {
+		return this.positions;
+	}
+
+	public void addRange(Cluster cluster) {
+		this.positions.addAll(cluster.getPositions());
+	}
 }
