e1dab92455b0b33583b431f1b04ab09d436b2de6
arian2ashk <arian_2ashk@yahoo.com>
2014-02-22 18:06:45 +0100

    dsfsdfasfdsd

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,16 +6,11 @@ import Model.Cluster;
 import Model.ClusterCollection;
 import Model.Position;
 
-=======
->>>>>>> 2b4dba0c02f67d6489f46afe5d409ca5079e9bc6
 public class GameLogic implements IGameLogic {
 	private int x = 0;
 	private int y = 0;
 	private int playerID;
-<<<<<<< HEAD
 	ClusterCollection CC;
-=======
->>>>>>> 2b4dba0c02f67d6489f46afe5d409ca5079e9bc6
 
 	public GameLogic() {
 		// TODO Write your implementation for this method
@@ -26,10 +20,7 @@ public class GameLogic implements IGameLogic {
 		this.x = x;
 		this.y = y;
 		this.playerID = playerID;
-<<<<<<< HEAD
 		 CC=new ClusterCollection(x, y);
-=======
->>>>>>> 2b4dba0c02f67d6489f46afe5d409ca5079e9bc6
 		// TODO Write your implementation for this method
 	}
 
@@ -40,7 +31,6 @@ public class GameLogic implements IGameLogic {
 
 	public void insertCoin(int column, int playerID) {
 		// TODO Write your implementation for this method
-<<<<<<< HEAD
 		
 		
 		//get the x,y coordinates of the coin to insert
@@ -50,12 +40,6 @@ public class GameLogic implements IGameLogic {
 	public int decideNextMove() {
 		// TODO Write your implementation for this method
 		return 0;
-=======
->>>>>>> 2b4dba0c02f67d6489f46afe5d409ca5079e9bc6
 	}
 
-	public int decideNextMove() {
-		// TODO Write your implementation for this method
-		return 0;
-	}
 }
